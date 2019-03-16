c7ef8a207c671adea5b907145cbfadc5df7e65bc
Rimmo <rimmo.room@gmail.com>
2019-03-16 23:35:13 +0200

    Lisatud JSON parser ja timestamp converter, testitud isikuväljad kohaliku ilmaga

--- src/Ilmajaam.java
+++ src/Ilmajaam.java
@@ -6,21 +6,24 @@ public class Ilmajaam {
         System.out.println("Programm küsib linna nime ning tagastab antud linna ilmateate.\nKui sisestad sõna virmalised, saad teada, kas Eestis on hetkel lootust näha virmalisi.\nProgrammist väljumiseks kirjuta välju");
 
         while (true){
-        String sisestatakse = JOptionPane.showInputDialog(null, "Sisesta linna nimi ", "Linn",
-                JOptionPane.QUESTION_MESSAGE);
-        if (sisestatakse.equalsIgnoreCase("välju")){
-            break;
-        }
+            String sisestatakse = JOptionPane.showInputDialog(null, "Sisesta linna nimi ", "Linn",
+                    JOptionPane.QUESTION_MESSAGE);
+            if (sisestatakse.equalsIgnoreCase("välju")){
+                break;
+            }else if (sisestatakse.equalsIgnoreCase("virmalised")){
+                Linn linn = new Linn("Eesti");
+                System.out.println(linn.eestiVirmalised());
+            }else{
+                Linn linn = new Linn(sisestatakse);
+                System.out.println(linn);
+            }
 
-        if (sisestatakse.equalsIgnoreCase("virmalised")){
-            Linn linn = new Linn();
-            System.out.println(linn.eestiVirmalised());
-        }
 
+        /*
         Linn linn = new Linn();
         //linnameetodid kust saadakse ilma andmed kätte
-            System.out.println(/*linn.getIlmateade vmt*/);
-
+            System.out.println(/*linn.getIlmateade vmt);
+        */
     }
     }
 }
--- src/Linn.java
+++ src/Linn.java
@@ -1,22 +1,82 @@
-import java.net.*;
-import java.io.*;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.text.SimpleDateFormat;
 
 public class Linn {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Algus");
-        URL oracle = new URL("http://api.openweathermap.org/data/2.5/weather?q=Tartu,ee&APPID=d87e964760570c1332f3d5a453769811");
-        BufferedReader in = new BufferedReader(
-                new InputStreamReader(oracle.openStream()));
+    private String name;
+    private double lon;
+    private double lat;
+    private String weather;
+    private double temp;
+    private int pressure;
+    private int humidity;
+    private int visibility;
+    private double windSpeed;
+    private int windDeg;
+    private int clouds;
+    private long sunrise;
+    private long sunset;
+    private long dt; // aeg mil ilmateate andmeid uuendati
+    private int cityExict;
 
-        String inputLine;
-        while ((inputLine = in.readLine()) != null)
-            System.out.println(inputLine);
-        in.close();
-        System.out.println();
-        Virmalised virm = new Virmalised();
-        System.out.println(virm.päikesetuul(virm.puhastatud(getData("https://services.swpc.noaa.gov/text/ace-swepam.txt"))));
+    public Linn(String linn) throws Exception{
+        String ilmateade = String.format("http://api.openweathermap.org/data/2.5/weather?q=%s,ee&APPID=d87e964760570c1332f3d5a453769811", linn);
+        try {
+            JSONObject hetkeilm = new JSONObject(getData(ilmateade));
 
+            this.name = linn;
+            this.lon = hetkeilm.getJSONObject("coord").getDouble("lon");
+            this.lat = hetkeilm.getJSONObject("coord").getDouble("lat");
+            this.weather = hetkeilm.getJSONArray("weather").getJSONObject(0).getString("description");
+            this.temp = hetkeilm.getJSONObject("main").getDouble("temp");
+            this.pressure = hetkeilm.getJSONObject("main").getInt("pressure");
+            this.humidity = hetkeilm.getJSONObject("main").getInt("humidity");
+            this.visibility = hetkeilm.getInt("visibility");
+            this.windSpeed = hetkeilm.getJSONObject("wind").getDouble("speed");
+            this.windDeg = hetkeilm.getJSONObject("wind").getInt("deg");
+            this.clouds = hetkeilm.getJSONObject("clouds").getInt("all");
+            this.sunrise = hetkeilm.getJSONObject("sys").getLong("sunrise");
+            this.sunset = hetkeilm.getJSONObject("sys").getLong("sunset");
+            this.dt = hetkeilm.getLong("dt");
+            this.cityExict = 1;
+        }catch (FileNotFoundException teade){
+            this.name = linn;
+            this.cityExict = 0;
+        }
     }
+
+    @Override
+    public String toString() {
+
+        SimpleDateFormat sdf = new java.text.SimpleDateFormat("yyyy-MM-dd HH:mm:ss z");
+        sdf.setTimeZone(java.util.TimeZone.getTimeZone("GMT+2"));
+
+        if (cityExict <= 0){
+            return "\"" + name + "\" linna ei eksisteeri";
+        }else {
+            return "Linn{" +
+                    "lon=" + lon +
+                    ", lat=" + lat +
+                    ", weather='" + weather +
+                    ", temp=" + temp +
+                    ", pressure=" + pressure +
+                    ", humidity=" + humidity +
+                    ", visibility=" + visibility +
+                    ", windSpeed=" + windSpeed +
+                    ", windDeg=" + windDeg +
+                    ", clouds=" + clouds +
+                    ", sunrise=" + sdf.format(sunrise*1000L) +
+                    ", sunset=" + sdf.format(sunset*1000L) +
+                    ", dt=" + sdf.format(dt*1000L) +
+                    '}';
+        }
+    }
+
+
 
     public String[] eestiVirmalised() throws Exception{
         String url = "https://services.swpc.noaa.gov/text/ace-swepam.txt";
